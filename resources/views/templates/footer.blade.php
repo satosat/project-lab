477ad2cda5f13adb62984a607e47edbf7f464b5f
vass26 <vasthy.tanisya@binus.ac.id>
2022-10-20 16:37:01 +0700

    update footer

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -8,30 +8,31 @@
    
 </head>
 <body>
-
-    <div class="footer bg-dark text-center p-4">
-        <div class="title text-danger p-2">
-            <h3>MovieList</h3>
+    <footer class="footer bg-dark text-center p-4">
+        <div class="container">
+            <div class="title text-danger p-2">
+                <h3>MovieList</h3>
+            </div>
+            <div class="title_desc text-white p-2">
+                MovieList a website that contains list of movies
+            </div>
+            <div class="social_media p-2">
+                <img src="images/ig.png" alt="" srcset="" width="30" height="30" class="m-2"> 
+                <img src="images/twitter.png" alt="" srcset="" width="30" height="30" class="m-2">
+                <img src="images/fb.png" alt="" srcset="" width="30" height="30" class="m-2">
+                <img src="images/youtube.png" alt="" srcset="" width="30" height="30" class="m-2">
+            </div>
+            <div class="text-white p-2 d-flex justify-content-center" style="text-decoration: none">
+                <p class="m-2">Privacy Policy</p>
+                <p class="m-2">Terms of Service</p>
+                <p class="m-2">Contact Us</p>
+                <p class="m-2">About Us</p>
+            </div>
+            <div class="copyright text-white p-2">
+                Copyright &copy; 2021 MovieList All rights Reserved
+            </div>
         </div>
-        <div class="title_desc text-white p-2">
-            MovieList a website that contains list of movies
-        </div>
-        <div class="social_media p-2">
-            <img src="images/ig.png" alt="" srcset="" width="30" height="30" class="m-2"> 
-            <img src="images/twitter.png" alt="" srcset="" width="30" height="30" class="m-2">
-            <img src="images/fb.png" alt="" srcset="" width="30" height="30" class="m-2">
-            <img src="images/youtube.png" alt="" srcset="" width="30" height="30" class="m-2">
-        </div>
-        <div class="text-white p-2 d-flex justify-content-center" style="text-decoration: none">
-            <p class="m-2">Privacy Policy</p>
-            <p class="m-2">Terms of Service</p>
-            <p class="m-2">Contact Us</p>
-            <p class="m-2">About Us</p>
-        </div>
-        <div class="copyright text-white p-2">
-            Copyright &copy; 2021 MovieList All rights Reserved
-        </div>
-    </div>
+    </footer>
     
 </body>
 </html>
