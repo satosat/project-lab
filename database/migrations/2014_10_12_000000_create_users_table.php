6062e8d3443ad750fee5a380c3059db9c37371bb
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-11-01 12:57:39 +0700

    fixed registration + login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->uniqe();
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,63 +1,68 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('templates.master')
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+@section('title', 'Register')
 
-            <!-- Name -->
+@section('content')
+    <div class="container d-flex justify-content-center bg-dark">
+        <div>
+            <div class="mt-5 d-flex">
+                <p class="text-white fs-1 fw-semibold">Hello, Welcome back to MovieList</p>
+            </div>
             <div>
-                <x-input-label for="username" :value="__('Username')" />
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
 
-                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
-                    required autofocus />
+                    {{-- Username --}}
+                    <div>
+                        <x-input-label class="form-label text-white" for="username" :value="__('Username')" />
 
-                <x-input-error :messages="$errors->get('username')" class="mt-2" />
+                        <x-text-input id="username" class="block mt-1 w-full form-control" type="text" name="username"
+                            :value="old('username')" required autofocus />
+
+                        <x-input-error :messages="$errors->get('username')" class="mt-2 text-white" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label class="form-label text-white" for="email" :value="__('Email')" />
+
+                        <x-text-input id="email" class="block mt-1 w-full form-control" type="email" name="email"
+                            :value="old('email')" required />
+
+                        <x-input-error :messages="$errors->get('email')" class="mt-2 text-white" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label class="form-label text-white" for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full form-control" type="password" name="password"
+                            required autocomplete="new-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2 text-white" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label class="form-label text-white" for="password_confirmation" :value="__('Confirm Password')" />
+
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full form-control" type="password"
+                            name="password_confirmation" required />
+
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-white" />
+                    </div>
+
+                    <div class="d-flex justify-content-center mt-4">
+                        <button type="submit" class="btn btn-primary w-100">Register</button>
+                    </div>
+                </form>
             </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    required />
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <div class="d-flex justify-content-center mt-3">
+                <span class="text-white">Already have an account?
+                    <a href="{{ route('login') }}" class="text-danger text-decoration-none">Login Now
+                    </a>
+                </span>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                    autocomplete="new-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                    name="password_confirmation" required />
-
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+@endsection
