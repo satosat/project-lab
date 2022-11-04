43a06bd92700020ef801f6ae6ad39b980a982fd7
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-11-04 23:03:07 +0700

    deleted breeze-installed files

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,4 @@ Route::get('/movies/create', [MovieController::class, 'create']);
 Route::post('/movies', [MovieController::class, 'store'])->name("add book");
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name("show book");
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__ . '/auth.php';
+// require __DIR__ . '/auth.php';
