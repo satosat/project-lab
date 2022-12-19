e7bb02aa4d8ed7a631c2ca8cbf22ba40b827a7db
Timotius Satrio Wicaksono <satriotimotius@gmail.com>
2022-12-19 09:09:47 +0700

    create movie controller working

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,13 @@ Route::post('/login', [LoginController::class, 'authenticate'])->name('authentic
 Route::redirect('/', '/movies');
 Route::get('/movies', [MovieController::class, 'index'])->name('home');
 Route::get('/movies/create', [MovieController::class, 'create'])->name('create new movie');
+Route::post('/movies', [MovieController::class, 'store'])->name("movies.store");
 Route::get('/movies/{id}/edit', [MovieController::class, 'edit'])->name('edit movie');
 Route::post('/movies', [MovieController::class, 'store'])->name("add movie");
 Route::get('/movies/{id}', [MovieController::class, 'show'])->name("show movie");
 
+// Actors
 Route::get('/actors', [ActorController::class, 'index'])->name('actors');
-Route::get('/actors/create', [ActorController::class, 'create'])->name('add actor');
-Route::get('/actors/{id}', [ActorController::class, 'show'])->name('show actor');
+Route::get('/actors/create', [ActorController::class, 'create'])->name('actors.create');
+Route::post('/actors', [ActorController::class, 'store'])->name('actors.store');
+Route::get('/actors/{id}', [ActorController::class, 'show'])->name('actors.show');
